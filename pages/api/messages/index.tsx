fc9370a7e1b99a380e0a5513a805b2989683d99c
Dennis <dennisthompson195@gmail.com>
2024-01-09 00:05:36 -0600

    deny GET requests to the API

--- pages/api/messages/index.tsx
+++ pages/api/messages/index.tsx
@@ -1,6 +1,7 @@
 import Messages from 'models/message'
 import dbConnect from '../../../lib/dbConnect'
 
+const restricted = <h1>If you are seeing this message, it is because you do not have the proper permissions to view this resource. < br /> If you believe this message to an error, please contact our support team.</h1>
 export default async function handler(req, res) {
   const { method } = req
 
@@ -10,7 +11,7 @@ export default async function handler(req, res) {
     case 'GET':
       try {
         const message = await Messages.find({}) /* find all the data in our database */
-        res.status(200).json({ success: true, data: message })
+        res.status(200).json({ success: true, data: false})
       } catch (error) {
         res.status(400).json({ success: false })
       }
