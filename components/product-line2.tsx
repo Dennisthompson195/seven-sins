dadd00ff460e6040b3a46b1a4d9e7586a6313b5b
Dennis <dennisthompson195@gmail.com>
2024-01-09 14:51:01 -0600

    link update

--- components/product-line2.tsx
+++ components/product-line2.tsx
@@ -9,7 +9,7 @@ const ProductlineTwo = () => {
           <section className=' h-full  p-6 max-sm:-mt-4 font-semibold 
            grid max-w-screen -gap-x-0 gap-y-8  mx-auto items-center text-center justify-center mt-6 mb-12 md:grid-cols-4 grid-rows-3 :grid-cols-2 xs:max-md:grid-cols-2  lg:w-2/3'>
           <div className='block relative lg:h-56 lg:w-56 xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
-               place-content-center justify-center mx-auto text-center'><Link href="/shop/chips"><Image
+               place-content-center justify-center mx-auto text-center'><Link href="/product/cara-cara-orange"><Image
              src='/Cara Cara.png'
              width={200}
              height={200}
@@ -19,7 +19,7 @@ const ProductlineTwo = () => {
 
              <div className='block relative lg:h-56 lg:w-56 xs:w-40 xs:h-40 mx-auto xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
               place-content-center justify-center text-center'><Link
-             href='/shop'> <Image
+             href='/product/orange-chips'> <Image
              src='/Naval 1.png'
              width={200}
              height={200}
@@ -28,7 +28,7 @@ const ProductlineTwo = () => {
            /></Link><h2 className="relative">Naval Orange</h2></div>
 
              <div className='block relative lg:h-56 lg:w-56 mx-auto xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none
-             place-content-center justify-center text-center'><Link href="/product/pant"><Image
+             place-content-center justify-center text-center'><Link href="/product/anjou-pear"><Image
              src='/Pear 1.png'
              width={200}
              height={200}
@@ -57,7 +57,7 @@ const ProductlineTwo = () => {
            /></Link><h2 className="relative">Lime</h2></div>
 
 <div className='block relative lg:h-56 lg:w-56 mx-auto mt-0 xs:-mt-8 xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none
-               place-content-center justify-center text-center'><Link href="/shop"><Image
+               place-content-center justify-center text-center'><Link href="/product/pineapple"><Image
              src='/Pineapple 4.png'
              width={200}
              height={200}
