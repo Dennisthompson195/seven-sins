4ce521911e7c26f2818a5171ded7b254e6527db7
Dennis <dennisthompson195@gmail.com>
2024-02-02 15:36:28 -0600

    pl-update

--- components/product-line.tsx
+++ components/product-line.tsx
@@ -7,27 +7,27 @@ const Productline = () => {
       <h1 className='text-center text-black font-medium relative text-base'>OUR PRODUCTS</h1>
     <hr className=' bg-black relative top-9 text-black mx-auto justify-center items-center text-center border-neutral-400 rounded-3xl opacity-80 w-8 -mt-8 border-y-2  font-extrabold'/>
           <section className=' h-full  p-6 max-sm:-mt-4 font-semibold 
-           grid max-w-screen gap-x-0 gap-y-8  mx-auto items-center text-center justify-center mt-6 mb-12 md:grid-cols-4 max-sm:grid-rows-3 md:grid-rows-2 :grid-cols-2 xs:max-md:grid-cols-2  lg:w-2/3'>
-          <div className='block relative lg:h-56 lg:w-56 xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
+           grid max-w-screen gap-x-0 gap-y-8  mx-auto items-center text-center justify-center mt-6 mb-12 md:grid-cols-2 max-sm:grid-rows-2 md:grid-rows-1 :grid-cols-2 xs:max-md:grid-cols-2  lg:w-1/2'>
+          <div className='block relative lg:h-[300px] lg:w-[200px] xs:w-40 xs:h-40  xl:hover:border-neutral-300 xs:max-xl:border-none 
                place-content-center justify-center mx-auto text-center'><Link href="/product/cara-cara-orange"><Image
-             src='/Cara Cara.png'
+             src='/Pina-p.png'
              width={200}
              height={200}
              alt='logo'
-             className=' block mt-2 relative -mb-6 mx-auto items-center justify-center'
-           /></Link><h2 className="relative">Cara Cara Orange</h2></div>
+             className=' block mt-2 relative -mb-2 mx-auto items-center justify-center'
+           /></Link><h2 className="relative">Fruit Chips</h2></div>
 
-             <div className='block relative lg:h-56 lg:w-56 xs:w-40 xs:h-40 mx-auto xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
+             <div className='block relative lg:h-[300px] lg:w-[200px] xs:w-40 xs:h-40 mx-auto  xl:hover:border-neutral-300 xs:max-xl:border-none 
               place-content-center justify-center text-center'><Link
              href='/product/orange-chips'> <Image
-             src='/Naval 1.png'
+             src='/BOS.png'
              width={200}
              height={200}
              alt='logo'
-             className='block relative mt-2 -mb-6 mx-auto items-center justify-center '
-           /></Link><h2 className="relative">Naval Orange</h2></div>
+             className='block relative mt-2 -mb-2 mx-auto items-center justify-center '
+           /></Link><h2 className="relative">Syrups</h2></div>
 
-             <div className='block relative lg:h-56 lg:w-56 mx-auto xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none
+             {/*<div className='block relative lg:h-56 lg:w-56 mx-auto xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none
              place-content-center justify-center text-center'><Link href="/product/anjou-pear"><Image
              src='/Pear 1.png'
              width={200}
@@ -81,7 +81,7 @@ const Productline = () => {
              height={200}
              alt='logo'
              className=' block mt-2 relative -mb-6 mx-auto items-center justify-center'
-           /></Link><h2 className=" relative  items-center mx-auto justify-center  text-center ">Cara Cara- Brulee</h2></div>
+           /></Link><h2 className=" relative  items-center mx-auto justify-center  text-center ">Cara Cara- Brulee</h2></div>*/}
             </section>
     </div>
   )
--- components/product-line2.tsx
+++ components/product-line2.tsx
@@ -3,31 +3,31 @@ import Link from "next/link"
 
 const ProductlineTwo = () => {
   return (
-    <div className="mb-28 lg:mt-10 xs:-mt-48 sm:mt-2 mx-auto w-screen items-center justify-center">
+    <div className="mb-28 lg:mt-10 xs:-mt-44 sm:mt-2 mx-auto w-screen items-center justify-center">
       <h1 className='text-center text-black font-medium relative bottom-0 text-base'>OUR PRODUCTS</h1>
     <hr className=' bg-black  text-black mx-auto justify-center items-center text-center border-neutral-400 rounded-3xl opacity-80 w-8 mt-2 border-y-2  font-extrabold'/>
           <section className=' h-full  p-6 max-sm:-mt-4 font-semibold 
-           grid max-w-screen -gap-x-0 gap-y-8  mx-auto items-center text-center justify-center mt-6 mb-12 md:grid-cols-4 grid-rows-3 :grid-cols-2 xs:max-md:grid-cols-2  lg:w-2/3'>
-          <div className='block relative lg:h-56 lg:w-56 xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
+           grid max-w-screen -gap-x-0 gap-y-0  mx-auto items-center text-center justify-center mt-6 mb-12 md:grid-cols-4 grid-rows-1 :grid-cols-2 xs:max-md:grid-cols-2  lg:w-2/3'>
+          <div className='block relative h-[300px] w-[200px]  xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
                place-content-center justify-center mx-auto text-center'><Link href="/product/cara-cara-orange"><Image
-             src='/Cara Cara.png'
+             src='/Pina-P.png'
              width={200}
              height={200}
              alt='logo'
-             className=' block mt-2 relative -mb-6 mx-auto items-center justify-center'
-           /></Link><h2 className="relative">Cara Cara Orange</h2></div>
+             className=' block mt-2 relative -mb-2 mx-auto items-center justify-center'
+           /></Link><h2 className="relative">Fruit Chips</h2></div>
 
-             <div className='block relative lg:h-56 lg:w-56 xs:w-40 xs:h-40 mx-auto xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
+             <div className='block relative h-[300px] w-[200px]  mx-auto xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none 
               place-content-center justify-center text-center'><Link
              href='/product/orange-chips'> <Image
-             src='/Naval 1.png'
+             src='/BOS.png'
              width={200}
              height={200}
              alt='logo'
-             className='block relative mt-2 -mb-6 mx-auto items-center justify-center '
-           /></Link><h2 className="relative">Naval Orange</h2></div>
+             className='block relative mt-2 -mb-2 mx-auto items-center justify-center '
+           /></Link><h2 className="relative">Syrups</h2></div>
 
-             <div className='block relative lg:h-56 lg:w-56 mx-auto xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none
+             {/*<div className='block relative lg:h-56 lg:w-56 mx-auto xs:w-40 xs:h-40 xl:border xl:border-solid xl:border-neutral-200 xl:hover:border-neutral-300 xs:max-xl:border-none
              place-content-center justify-center text-center'><Link href="/product/anjou-pear"><Image
              src='/Pear 1.png'
              width={200}
@@ -81,7 +81,7 @@ const ProductlineTwo = () => {
              height={200}
              alt='logo'
              className=' block mt-2 relative -mb-6 mx-auto items-center justify-center'
-           /></Link><h2 className=" relative  items-center mx-auto justify-center  text-center ">Cara Cara- Brulee</h2></div>
+           /></Link><h2 className=" relative  items-center mx-auto justify-center  text-center ">Cara Cara- Brulee</h2></div>*/}
 
             </section>
     </div>
