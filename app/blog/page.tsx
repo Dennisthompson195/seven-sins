2af9ee73940134dbbb99b5662179af592c916202
Dennis <dennisthompson195@gmail.com>
2024-02-23 00:01:18 -0600

    gg

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -17,7 +17,7 @@ export default function BlogPage() {
         Cocktail Recipes & More
       </h1>
       <p className='lg:w-1/2 max-lg:w-3/4  grid mx-auto text-lg font-medium'>Thirsty or looking for inspiration to create something new? We have several delicious recipes that are sure to have your guests talking about your drinks for weeks!</p>
-      <section className='grid max-lg:grid-cols-2 lg:grid-cols-3 mx-auto  place-content-center justify-center items-center w-5/6'>
+      <section className='grid max-lg:grid-cols-2 lg:grid-cols-3 mx-auto  place-content-center justify-center items-center lg:w-5/6 max-lg:w-11/12'>
       {allBlogs
         .sort((a, b) => {
           if (
